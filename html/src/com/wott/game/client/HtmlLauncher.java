c10a803c86e26019bcb0b9da9a3f4eb96dae609e
student <student@PC01.cis.com>
2017-05-01 12:02:11 -0500

    LibGDX stuff

---
+++ html/src/com/wott/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.wott.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.wott.game.WOTT;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WOTT();
+        }
+}
